ffd09744c2770fe05cae77eebc8f1109be525c5b
Elihan303 <elihantejada@gmail.com>
2023-01-03 17:48:20 -0400

    fix: config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://Elihan303.github.io",
-  base: "/Landing-page-astro",
+  base: "/Elihan-portafolio",
 });
